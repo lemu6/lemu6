41708a5a9a3d5ff1f0fb8c6727fd0e90df5d9f5f
LemuDev <lemu200011@gmail.com>
2024-03-16 20:49:06 -0300

    Fisrt commit

---
+++ app/Controllers/Produtos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Produtos extends BaseController
+{
+    public function Listar()
+    {
+        echo view('templates/header');
+        echo view('produtos/listar');
+        echo view('templates/footer');
+    }
+}
